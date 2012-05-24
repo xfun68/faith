56075c241fd4b154c697bcc7248541c246ebab24
Xiaofeng Wang <xfwang@thoughtworks.com>
2012-05-24 13:23:49 -0300

    rails new . --skip-active-record --skip-test-unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Faith::Application.config.secret_token = '6c055002b9b670347cd50022dd5e2fcdb565318f8ea9279d1ca8b8b61eb5c9088b45752ebf6cc2980f71d34de12dc21373065d6385c92cec4e24f91f3aa090a7'
